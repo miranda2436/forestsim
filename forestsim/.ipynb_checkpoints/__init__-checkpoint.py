4b6c6f9e7a11bcbf2d55c8e1e6e49ca9b5cd74fb
miranda2436 <mw881@cam.ac.uk>
2025-11-23 15:27:41 +0000

    add unsupervised random forest result

---
+++ forestsim/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+"""    We aim to create similarity measurements without preprocessing on any kind of data.
+"""
+
+__version__ = "0.1"
+
+def aim():
+    return ("This is a work in progress in the __init__.py file.")
